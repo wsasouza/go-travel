7f7242c024942d4fc5bc9272f76152ad2fe9b953
wsasouza <wsasouza@hotmail.com>
2022-03-07 18:19:11 -0300

    Feat: Create Components and Api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './style.js';
 
-const Header = () => {
+const Header = ({ onPlaceChanged, onLoad }) => {
   const classes = useStyles();
 
   return (
@@ -17,14 +17,14 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Explore novos lugares
           </Typography>
-          {/*<Autocomplete>*/}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder="Pesquisar…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-          {/*</Autocomplete>*/}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
